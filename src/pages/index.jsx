eb0c68cb47d1740b59656a7fbad2d4181eb76fac
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2024-01-25 14:53:08 +0900

    追加、削除機能　実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,41 @@
 import { useState } from "react"
 
 export default function Home() {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["Todoです1", "Todoです2"])
   const [completeTodos, setCompleteTodos] = useState(["Todoでした1", "Todoでした2"])
+
+  const onChangeTodoText = e => setTodoText(e.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  }
+  const onClickDelete = index => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  }
+
   
   return (
     <>
       <div className="input-area">
-        <input type="text" placeholder="todoを入力" />
-        <button>追加</button>
+        <input type="text" placeholder="todoを入力" value={todoText} onChange={ e => onChangeTodoText(e)} />
+        <button onClick={onClickAdd}>追加</button>
       </div>
 
       <div className="incomplete-area">
         <p className="title">未完了のtodo</p>
         <ul>
-          {incompleteTodos.map(incompleteTodo => (
+          {incompleteTodos.map((incompleteTodo, index) => (
             <li key={incompleteTodo}>
               <div className="list-row">
                 <p className="todo-item">{incompleteTodo}</p>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             </li>
           ))}
